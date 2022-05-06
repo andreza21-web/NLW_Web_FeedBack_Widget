ed897bf901b8f708cf950cbfb674b2cf3d684d75
andreza <andreza.21pessoa@gmail.com>
2022-05-06 19:37:31 -0300

    feedback types button

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,19 +1,68 @@
 import { CloseButton } from "./CloseButton";
+import bugImageUrl from '../assets/bug.svg';
+import ideaImageUrl from '../assets/idea.svg';
+import thoughtImageUrl from '../assets/thought.svg';
+import { useState } from "react";
+
+const feedbackTypes = {
+    BUG: {
+        title: 'Problema',
+        image: {
+            source: bugImageUrl,
+            alt: 'imagem de um inseto'
+        },
+    },
+    IDEA: {
+        title: 'Ideia',
+        image: {
+            source: ideaImageUrl,
+            alt: 'imagem de uma lampada'
+        },
+    },
+    OTHER: {
+        title: 'Outro',
+        image: {
+            source: thoughtImageUrl,
+            alt: 'imagem de uma nuvem'
+        },
+    }
+};
+
+type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
+
+    const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+
+
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4  flex flex-col items-center shadow-lg w-[cal(100vw-2rem)] md:w-auto" >
-           <header>
-               <span className="text-xl leading-6">Deixe seu feedback</span>
+            <header>
+                <span className="text-xl leading-6">Deixe seu feedback</span>
 
-               <CloseButton/>
-           </header>
+                <CloseButton />
+            </header>
 
-           <p>Hello word</p>
+            <div className="flex py-8 gap-2 w-full">
+                {Object.entries(feedbackTypes).map(([key, value]) => {
 
-           <footer className="text-xs text-neutral-400">
-           Feito com ♥ por <a className="underline underline-offset-1" href="https://github.com/andreza21-web">Andreza</a>
-          </footer>
+                    return (
+                        <button
+                            key={key}
+                            className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+                            onClick={() => setFeedbackType(key as FeedbackType)}
+                            type="button"
+                        >
+                            <img src={value.image.source} alt={value.image.alt} />
+                            <span>{value.title}</span>
+                        </button>
+                    );
+                })}
+            </div>
+            
+            <footer className="text-xs text-neutral-400">
+                Feito com ♥ por <a className="underline underline-offset-1" href="https://github.com/andreza21-web">Andreza</a>
+            </footer>
         </div>
     );
 }
