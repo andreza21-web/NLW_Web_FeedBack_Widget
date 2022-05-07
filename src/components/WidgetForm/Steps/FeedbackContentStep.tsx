35f688713eb37d18d3e5b3cc0a04dac3d98a824b
andreza <andreza.21pessoa@gmail.com>
2022-05-07 14:44:16 -0300

    button Enviar Feedback

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,4 +1,4 @@
-import { ArrowLeft } from "phosphor-react";
+import { ArrowLeft, Camera } from "phosphor-react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
 
@@ -7,7 +7,7 @@ interface FeedbackContentStepProps {
 }
 
 export function FeedbackContentStep({ feedbackType }: FeedbackContentStepProps) {
-    
+
     const feedbackTypeInfo = feedbackTypes[feedbackType];
 
     return (
@@ -15,24 +15,43 @@ export function FeedbackContentStep({ feedbackType }: FeedbackContentStepProps) 
             <header>
 
                 <button type="button" className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100">
-                    <ArrowLeft weight="bold" className="w-4 h-4 "/>
+                    <ArrowLeft weight="bold" className="w-4 h-4 " />
                 </button>
 
                 <span className="text-xl leading-7 flex items-center gap-2">
-                    <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt}  className="w-6 h-6"/>
+                    <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} className="w-6 h-6" />
                     {feedbackTypeInfo.title}
                 </span>
-                
+
                 <CloseButton />
 
             </header>
 
             <form className="m-4 w-full">
-                <textarea 
-                
-                className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100  border-zinc-600 bg-transparent rounded-md "
-                placeholder="Conte com detalhes o que está acontecendo....."
+                <textarea
+                    className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100  border-zinc-600 bg-transparent rounded-md 
+                focus:border-t-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent"
+                    placeholder="Conte com detalhes o que está acontecendo....."
                 />
+
+                <footer className="flex gap-2 mt-2">
+
+                    <button
+                        type="button"
+                        className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 focus: outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+                    >
+                        <Camera className="w-6 h-6" />
+                    </button>
+
+                    <button
+                        type="submit"
+                        className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 
+                    focus: outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors "
+                    >
+                        Enviar Feedback
+                    </button>
+
+                </footer>
             </form>
         </>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,19 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        brand:{
-          500:'#8257e6',
+        brand: {
+          300: '#8257e6',
+          500: '#8257e6',
         }
+      },
+      borderRadius: {
+        md: '4px',
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar'),
+  ],
+
 }
